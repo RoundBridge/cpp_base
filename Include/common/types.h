8e642b31bc806fcff1462d6fb0bf4cb1699f9bde
RoundBridge <410308507@qq.com>
2020-06-26 20:47:34 +0800

    second commit

---
+++ Include/common/types.h
@@ -0,0 +1,71 @@
+/******************************************************************************
+FileName: 		types.h
+Description:   	data type definition
+Author:   		Round Bridge
+Date: 			2020.1.1
+Modification History:
+					2020.1.1 create file
+Version: 		0.0.1
+******************************************************************************/
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+#include <platform.h>
+
+#define NULL    0
+#define OK      0
+#define TRUE	1
+#define FALSE	0
+
+
+#ifdef PLATEFORM_X86_32
+
+#define INT_MINI     (-2147483648)
+#define INT_MAXI     (2147483647)
+
+typedef unsigned char		uint8;
+typedef unsigned short		uint16;
+typedef unsigned int		uint32;
+typedef unsigned int		uint;
+typedef unsigned long long 	uint64;
+typedef unsigned char*		puint8;
+typedef unsigned short*		puint16;
+typedef unsigned int*		puint32;
+typedef unsigned int*		puint;
+typedef unsigned long long*	puint64;
+typedef signed char			sint8;
+typedef short				sint16;
+typedef int					sint32;
+typedef int					sint;
+typedef long long 			sint64;
+typedef signed char*		psint8;
+typedef short*				psint16;
+typedef int*				psint;
+typedef int*				psint32;
+typedef long long*		 	psint64;
+typedef void*				pvoid;
+
+typedef volatile unsigned char			uint8v;
+typedef volatile unsigned char*			puint8v;
+typedef volatile unsigned short			uint16v;
+typedef volatile unsigned short*		puint16v;
+typedef volatile unsigned int			uint32v;
+typedef volatile unsigned int*			puint32v;
+typedef volatile unsigned long long		uint64v;
+typedef volatile unsigned long long*	puint64v;
+
+typedef volatile signed char			sint8v;
+typedef volatile signed char*			psint8v;
+typedef volatile short					sint16v;
+typedef volatile short*					psint16v;
+typedef volatile int					sint32v;
+typedef volatile int*					psint32v;
+typedef volatile long long				sint64v;
+typedef volatile long long*				psint64v;
+
+
+#endif
+
+
+#endif
+
