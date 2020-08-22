a798d0ebd527cdc6cd06f5b18d00e6161f0e6786
RoundBridge <410308507@qq.com>
2020-08-22 16:32:16 +0800

    add two files

---
+++ Include/geometry/mgeometry.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+FileName: 		mgeometry.h
+Description:   	header file
+Author:   		Round Bridge
+Date: 			2020.8.22
+Modification History:
+					2020.8.22 create file
+Version: 		0.0.1
+******************************************************************************/
+
+#ifndef _MGEOMETRY_H_
+#define _MGEOMETRY_H_
+
+#include "types.h"
+
+namespace mgeometry
+{
+	class Geometry{
+	public:
+		virtual double length() const=0;
+		virtual double area() const=0;
+		virtual double volume() const=0;
+	};
+
+	template <class Type_Point>
+	class Point{
+	public:
+		Point(){x=(Type_Point)0; y=(Type_Point)0;}
+		Point(Type_Point a, Type_Point b){x=a; y=b;}
+		Point(const Point<Type_Point>& a){x=a.x; y=a.y;}
+		Point<Type_Point>& operator=(const Point<Type_Point>& rightside);
+		virtual ~Point(){}
+		Type_Point get_point_x() const;  //const表示不能改变调用对象的值
+		Type_Point get_point_y() const;
+		void set_point(const Point<Type_Point>& a);
+	private:
+		Type_Point x;
+		Type_Point y;
+	};
+
+}
+
+
+#endif
+
---
+++ Src/geometry/mgeometry.cpp
@@ -0,0 +1,47 @@
+/******************************************************************************
+FileName: 		mgeometry.cpp
+Description:   	mgeometry definition
+Author:   		Round Bridge
+Date: 			2020.8.22
+Modification History:
+					2020.8.22 create file
+Version: 		0.0.1
+******************************************************************************/
+
+#ifndef _MGEOMETRY_CPP_
+#define _MGEOMETRY_CPP_
+
+#include "mgeometry.h"
+
+namespace mgeometry
+{
+	//返回引用，允许像下面这样的层叠赋值语句a = b = c;
+	//层叠赋值语句之所以有效，是因为内置赋值运算符在
+	//起作用。重载时也做到和内置类型保持一致
+	template <class Type_Point>
+	Point<Type_Point>& Point<Type_Point>::operator=(const Point<Type_Point>& rightside){
+		this->x = rightside.x;
+		this->y = rightside.y;
+		return *this;
+	}
+	
+	template <class Type_Point>
+	Type_Point Point<Type_Point>::get_point_x() const{
+		return this->x;			
+	}
+
+	template <class Type_Point>
+	Type_Point Point<Type_Point>::get_point_y() const{
+		return this->y;			
+	}
+
+	template <class Type_Point>
+	void set_point(const Point<Type_Point>& a){
+		this->x = a.x;
+		this->y = a.y;
+		return;
+	}
+}
+
+#endif
+
